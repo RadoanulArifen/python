50bf5e3a775ce49615a9915a2886f34bc6433694
Arifen <113052888+ArifenFahim@users.noreply.github.com>
2025-09-29 00:40:49 +0600

    string

---
+++ pra1string.py
@@ -0,0 +1,14 @@
+a="This is a string"
+print(a)
+print(type(a))
+
+b="""This is a string for depending for the nation"""
+print(b)
+print(type(b))
+
+c='This is a len of this sentence'
+print(len(c)) #lenth of the sentence
+print(c[len(c)-1]) #print last letter longcut
+print(a[-1]) #print last letter shortcut
+
+
